f5a6e03cac32ac36002b638e8d257744bb14c0c2
Hajar Salamah <programer4455@gmail.com>
2020-08-26 20:28:55 +0300

    This frist kotlin project  That Intro to Type and varable from hajar slamah

---
+++ src/TypeIntro.kt
@@ -0,0 +1,22 @@
+fun main(){
+    var expereincePoint:Int =5;
+      var playerName :String="Estragon";
+        expereincePoint+=5;
+         println("expereincePoint is :"+expereincePoint );
+    println("playerName is :"+playerName);
+
+    var hasSteed :Int=2;
+    if(hasSteed==1)  {
+        println(" Th ride : dragon");
+        hasSteed=1;
+    } else if(hasSteed==2) {
+        println("Th ride : mintore");
+        hasSteed=2;
+    }  else{
+        println("Have not Yet")}
+         println(" Magic mirore is 0__0 :"+playerName.reversed())
+}
+
+
+class TypeIntro {
+}
